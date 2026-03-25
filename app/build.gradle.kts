a122ba2c7cd17944f1220d50328c3a222d670bd2
Admin <vadimsuman55@gmail.com>
2026-03-25 23:03:16 +0700

    Add ViewModel Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
--- app/src/main/java/com/shumtech/movies/model/RetrofitClient.kt
+++ app/src/main/java/com/shumtech/movies/model/RetrofitClient.kt
@@ -2,13 +2,15 @@ package com.shumtech.movies.model
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://www.omdbapi.com/"
-    const val API_KEY = "288914b9"   // ваш ключ, полученный от OMDb
+    const val API_KEY = "288914b9"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -20,11 +22,16 @@ object RetrofitClient {
         .readTimeout(30, TimeUnit.SECONDS)
         .build()
 
+    // Создаём Moshi с поддержкой Kotlin
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
     val instance: MovieApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi)) // используем настроенный moshi
             .build()
             .create(MovieApi::class.java)
     }
