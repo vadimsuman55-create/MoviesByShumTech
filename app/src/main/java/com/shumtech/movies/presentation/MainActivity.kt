2e80e2109e467aa10c769330a36505a5d0c160d7
Admin <vadimsuman55@gmail.com>
2026-04-01 10:36:07 +0700

    Use UseCases in ViewModel and update factory

---
+++ app/src/main/java/com/shumtech/movies/presentation/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.shumtech.movies.presentation
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.shumtech.movies.data.MovieRepositoryImpl
+import com.shumtech.movies.domain.usecase.AddMovieUseCase
+import com.shumtech.movies.domain.usecase.DeleteSelectedMoviesUseCase
+import com.shumtech.movies.domain.usecase.GetMovieByIdUseCase
+import com.shumtech.movies.domain.usecase.GetMoviesUseCase
+import com.shumtech.movies.domain.usecase.SearchMoviesUseCase
+import com.shumtech.movies.domain.usecase.ToggleMovieSelectionUseCase
+import com.shumtech.movies.domain.usecase.UpdateMovieUseCase
+import com.shumtech.movies.model.MovieDatabase
+import com.shumtech.movies.model.RetrofitClient
+import com.shumtech.movies.presentation.theme.MoviesByShumTechTheme
+import com.shumtech.movies.presentation.viewmodel.MainViewModel
+import com.shumtech.movies.view.AddScreen
+import com.shumtech.movies.view.MainScreen
+import com.shumtech.movies.view.SearchScreen
+import com.shumtech.movies.domain.usecase.*
+import com.shumtech.movies.presentation.mvi.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MoviesByShumTechTheme {
+                val viewModel: MainViewModel = viewModel(
+                    factory = MainViewModelFactory(applicationContext)
+                )
+                val currentScreen by viewModel.currentScreen.collectAsState()
+                val mainState by viewModel.mainState.collectAsState()
+                val addState by viewModel.addState.collectAsState()
+                val searchState by viewModel.searchState.collectAsState()
+
+                when (currentScreen) {
+                    MainViewModel.Screen.MAIN -> MainScreen(
+                        state = mainState,
+                        onIntent = { viewModel.processIntent(it) }
+                    )
+
+                    MainViewModel.Screen.ADD -> AddScreen(
+                        state = addState,
+                        onIntent = { viewModel.processIntent(it) }
+                    )
+
+                    MainViewModel.Screen.SEARCH -> SearchScreen(
+                        state = searchState,
+                        onIntent = { viewModel.processIntent(it) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+class MainViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            // Получаем зависимости data слоя
+            val database = MovieDatabase.getDatabase(context)
+            val movieDao = database.movieDao()
+            val api = RetrofitClient.instance
+            val repository = MovieRepositoryImpl(movieDao, api)  // реализация репозитория
+
+            // Создаём UseCase'ы
+            val getMoviesUseCase = GetMoviesUseCase(repository)
+            val addMovieUseCase = AddMovieUseCase(repository)
+            val toggleSelectionUseCase = ToggleMovieSelectionUseCase(repository)
+            val deleteSelectedUseCase = DeleteSelectedMoviesUseCase(repository)
+            val searchMoviesUseCase = SearchMoviesUseCase(repository)
+            val getMovieByIdUseCase = GetMovieByIdUseCase(repository)
+            val updateMovieUseCase = UpdateMovieUseCase(repository)
+
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(
+                getMoviesUseCase,
+                addMovieUseCase,
+                toggleSelectionUseCase,
+                deleteSelectedUseCase,
+                searchMoviesUseCase,
+                getMovieByIdUseCase,
+                updateMovieUseCase
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
