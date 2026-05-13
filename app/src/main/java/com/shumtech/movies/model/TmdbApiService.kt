39a61a6cddbac2cf793a2c038c4c536c2158e6dd
Admin <vadimsuman55@gmail.com>
2026-05-13 10:32:07 +0700

    TMDB API клиент

--- app/src/main/java/com/shumtech/movies/model/TmdbApiService.kt
+++ app/src/main/java/com/shumtech/movies/model/TmdbApiService.kt
@@ -1,4 +1,28 @@
 package com.shumtech.movies.model
 
-class TmdbApiService {
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbApiService {
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieVideos(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "ru-RU"
+    ): TmdbVideoResponse
 }
+
+data class TmdbVideoResponse(
+    val results: List<TmdbVideo>
+)
+
+data class TmdbVideo(
+    val id: String,
+    val key: String,        // YouTube video ID
+    val name: String,
+    val site: String,       // "YouTube"
+    val type: String,       // "Trailer", "Teaser" и т.д.
+    val official: Boolean
+)
